899fabffc7ddccf84874a5079af42c103d1d0469
patrickCoded <innocentjosiah57@gmail.com>
2023-01-03 21:01:00 +0000

    initial commit of whitelist dapp

--- hardhat-tutorial/contracts/whitelist.sol
+++ hardhat-tutorial/contracts/whitelist.sol
@@ -1,35 +1,36 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
+contract Whitelist {
 
-// create the whitelist contract
+    // Max number of whitelisted addresses allowed
+    uint8 public maxWhitelistedAddresses;
 
-contract Whitelist{
-    // number of allowed address in the whitelist
-    uint8 public maxWhiteListedAddresses;
+    // Create a mapping of whitelistedAddresses
+    // if an address is whitelisted, we would set it to true, it is false by default for all other addresses.
+    mapping(address => bool) public whitelistedAddresses;
 
-    // create a variable for the number of address whitelisted
-    uint8 public numofWhitelistedAddress;
+    // numAddressesWhitelisted would be used to keep track of how many addresses have been whitelisted
+    // NOTE: Don't change this variable name, as it will be part of verification
+    uint8 public numAddressesWhitelisted;
 
-    // create a map of addres with a bolean value, set it to a public variable called whitelistedaddress
-    // by default each address is set to false 
-    mapping(address => bool) public whiteListedAddress;
-    
-    // craete a constructor to set the value of maxlistedAddressees
-    constructor ( uint8 _maxWhiteListedAddresses){
-     maxWhiteListedAddresses  = _maxWhiteListedAddresses;
+    // Setting the Max number of whitelisted addresses
+    // User will put the value at the time of deployment
+    constructor(uint8 _maxWhitelistedAddresses) {
+        maxWhitelistedAddresses =  _maxWhitelistedAddresses;
     }
 
-    // craete a function to add addresses
-    function addAddressToWhitelist() public{
-        // check if address is already in whitelist
-        // check if whitelistedaddress is true, if it, then the address has already been added, if it is false proceed to add the address
-        require(!whiteListedAddress[msg.sender], 'addres already added to whitelist');
-        // check if number of whitelisted address exceeds 
-        require(numofWhitelistedAddress < maxWhiteListedAddresses, 'exceed the number of allowed whitelisted addresses');
-        // set the address to true
-        whiteListedAddress[msg.sender] = true;
-        // increment the number of whitelisted address
-        numofWhitelistedAddress +=1;
-
+    /**
+        addAddressToWhitelist - This function adds the address of the sender to the
+        whitelist
+     */
+    function addAddressToWhitelist() public {
+        // check if the user has already been whitelisted
+        require(!whitelistedAddresses[msg.sender], "Sender has already been whitelisted");
+        // check if the numAddressesWhitelisted < maxWhitelistedAddresses, if not then throw an error.
+        require(numAddressesWhitelisted < maxWhitelistedAddresses, "More addresses cant be added, limit reached");
+        // Add the address which called the function to the whitelistedAddress array
+        whitelistedAddresses[msg.sender] = true;
+        // Increase the number of whitelisted addresses
+        numAddressesWhitelisted += 1;
     }
 }
